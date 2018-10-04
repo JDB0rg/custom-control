59e4b8c68e8ece6e06b53fe2fdca4203c663122d
Jonah Bergevin <jonahbergevin@gmail.com>
2018-10-04 10:00:09 -0600

    first commit

---
+++ UIContol/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  UIContol
+//
+//  Created by Madison Waters on 10/2/18.
+//  Copyright © 2018 Jonah Bergevin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func updateRating(_ ratingControl: CustomControl) {
+        
+        
+        
+//        if customControl.value == 1 {
+//         title = "User Rating: \(customControl.value) star"
+//        } else {
+//           title = "User Rating: \(customControl.value) stars"
+//        }
+    }
+
+}
